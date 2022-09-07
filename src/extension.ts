3363d5fec18a6e5cdd32acf7264bc2df8fcf90ec
Joris MASSON <joris.masson@enalean.com>
2022-09-07 10:04:46 +0200

    refactor: push extension disposables in context.subscriptions
    No need to manage clean-up ourselves.

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import type { Disposable } from "vscode";
+import type { ExtensionContext } from "vscode";
 import { commands } from "vscode";
 import { HelloWorldCommand } from "./HelloWorldCommand";
-
-const extensionDisposables: Disposable[] = [];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 // arg: extension: ExtensionContext
-export function activate(): void {
+export function activate(context: ExtensionContext): void {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
 
     const helloWorld = commands.registerCommand("tuleap.helloWorld", HelloWorldCommand);
-    extensionDisposables.push(helloWorld);
+    context.subscriptions.push(helloWorld);
 }
 
-// this method is called when your extension is deactivated
+// this method is called when your extension is deactivated or uninstalled
 export function deactivate(): void {
-    extensionDisposables.forEach((disposable) => disposable.dispose());
+    // Clean-up extension-specific things
 }
