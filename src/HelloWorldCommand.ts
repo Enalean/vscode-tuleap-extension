81a22181e6d683a2368a4124eb1f17bc655a0de6
Joris MASSON <joris.masson@enalean.com>
2022-09-07 10:04:39 +0200

    feat: query Dev tuleap with axios

--- src/HelloWorldCommand.ts
+++ src/HelloWorldCommand.ts
@@ -1,32 +1,37 @@
-import type { QuickPickItem } from "vscode";
+// import type { QuickPickItem } from "vscode";
 import { Uri, workspace, window } from "vscode";
-import * as fs from "fs";
-import type { InputStep } from "./MultiStepInput";
-import { MultiStepInput } from "./MultiStepInput";
+import axios from "axios";
+// import * as fs from "fs";
+// import type { InputStep } from "./MultiStepInput";
+// import { MultiStepInput } from "./MultiStepInput";
 
-interface State {
-    title: string;
-    step: number;
-    totalSteps: number;
-    selectedFile: QuickPickItem;
-    name: string;
-    runtime: QuickPickItem;
-}
+// interface State {
+//     title: string;
+//     step: number;
+//     totalSteps: number;
+//     selectedFile: QuickPickItem;
+//     name: string;
+//     runtime: QuickPickItem;
+// }
 
-function shouldResume(): Promise<boolean> {
-    // Could show a notification with the option to resume.
-    return new Promise<boolean>(() => {
-        // noop
-    });
-}
+// function shouldResume(): Promise<boolean> {
+//     // Could show a notification with the option to resume.
+//     return new Promise<boolean>(() => {
+//         // noop
+//     });
+// }
 
-async function validateIsANumber(name: string): Promise<string | undefined> {
-    // TODO check is KO validation is not done
-    // ...validate...
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(name);
-    return name === "0123456789" ? "Artifact id must be a number" : undefined;
-}
+// async function validateIsANumber(name: string): Promise<string | undefined> {
+//     // TODO check is KO validation is not done
+//     // ...validate...
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
+//     console.log(name);
+//     return name === "0123456789" ? "Artifact id must be a number" : undefined;
+// }
+
+// Disable NodeJS checking TLS certificates altogether.
+// DO NOT USE IN PRODUCTION
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 function getBaseFolderToOpenDialog(): Uri {
     if (workspace.workspaceFolders !== undefined) {
@@ -35,9 +40,22 @@ function getBaseFolderToOpenDialog(): Uri {
     return Uri.file(".");
 }
 
-export const HelloWorldCommand = (): void => {
-    const quickPickItems: QuickPickItem[] = [];
+function createFile(field_id: number, personal_access_key: string): void {
+    axios
+        .post(`https://tuleap-web.tuleap-aio-dev.docker/api/v1/tracker_fields/${field_id}/files`, {
+            headers: { "X-Auth-AccessKey": personal_access_key },
+        })
+        .then(
+            (response) => {
+                console.log(response.status);
+            },
+            (reason) => {
+                console.error("Could not make an HTTP request: %s", reason);
+            }
+        );
+}
 
+export const HelloWorldCommand = (): void => {
     window
         .showOpenDialog({
             canSelectMany: false,
@@ -47,16 +65,27 @@ export const HelloWorldCommand = (): void => {
             defaultUri: getBaseFolderToOpenDialog(),
         })
         .then(
-            (selectedFiles) => {
-                if (!selectedFiles) {
+            (selected_files) => {
+                if (!selected_files) {
                     console.error("No file selected !");
                 }
-                console.log(selectedFiles);
+                console.log(selected_files);
+
+                // project_id = 107
+                // tracker_id = 68
+                const field_id = 1394;
+                // artifact_id = 6616
+                const personal_access_key = "<replace me by a Tuleap personal access key>";
+
+                console.log("Making HTTP request to Tuleap");
+
+                createFile(field_id, personal_access_key);
             },
             (reason) => {
-                console.error("Could not show the open file dialog: " + reason);
+                console.error("Could not show the open file dialog: %s", reason);
             }
         );
+    // const quickPickItems: QuickPickItem[] = [];
     // fs.readdir(".", (err, files: string[]) => {
     //     files.forEach((file) => {
     //         const uri = Uri.file(file);
