ad7e45041baacbc7bb88ea6bcee85f115c37941a
Joris MASSON <joris.masson@enalean.com>
2022-09-07 10:09:37 +0200

    feat: Unwrap and display the Restler error message
    Otherwise there is no way to know why a request failed

--- src/APIQuerier.ts
+++ src/APIQuerier.ts
@@ -22,6 +22,19 @@ const mapToRequest = (file: FileStatsRetrieved): PostFileRequest => ({
     file_type: file.file_type,
 });
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const unwrapRestlerError = (error: any): never => {
+    if ("response" in error) {
+        if ("data" in error.response) {
+            if ("error" in error.response.data) {
+                const restler_error = error.response.data.error;
+                throw new Error(`API Error: ${restler_error.code} ${restler_error.message}`);
+            }
+        }
+    }
+    throw error;
+};
+
 const CHUNK_SIZE = 67108864; // = 64 MiB. Number of bytes held in memory at a time during file upload;
 
 export type APIQuerier = {
@@ -44,27 +57,28 @@ export const APIQuerier = (tuleap_base_uri: string, personal_access_key: string)
                     "Content-type": "application/json",
                 },
             })
-            .then((response) => ({
-                handle: file.handle,
-                file_id: response.data.id,
-                file_name: file.file_name,
-                file_size: file.file_size,
-                file_type: file.file_type,
-                upload_href: response.data.upload_href,
-            }));
+            .then(
+                (response) => ({
+                    handle: file.handle,
+                    file_id: response.data.id,
+                    file_name: file.file_name,
+                    file_size: file.file_size,
+                    file_type: file.file_type,
+                    upload_href: response.data.upload_href,
+                }),
+                unwrapRestlerError
+            );
     },
 
     uploadFile: (file): Promise<FileUploaded> => {
         const upload_url = new URL(file.upload_href, tuleap_base_uri);
 
         return new Promise<FileUploaded>((resolve, reject) => {
-            //TODO: feature request being able to pass number values as metadata. String type is rejected by Tuleap's Restler validation (for `file_size`)
             const uploader = new Upload(file.handle.createReadStream({ start: 0 }), {
                 uploadUrl: upload_url.href,
                 headers: { "X-Auth-AccessKey": personal_access_key },
                 // Note that tus-js-client's documentation specifies to avoid setting chunkSize and uploadSize unless forced to.
                 // We are forced to set them, it does not look like the detection of ReadableStream worked in our case.
-                //TODO: minimize the reproduction and report the issue
                 chunkSize: CHUNK_SIZE,
                 uploadSize: file.file_size,
                 onError: (error): void => {
@@ -81,15 +95,19 @@ export const APIQuerier = (tuleap_base_uri: string, personal_access_key: string)
     attachFileToArtifact: (artifact_id, field_id, file_id): Promise<void> => {
         const url = new URL(`/api/v1/artifacts/${artifact_id}`, tuleap_base_uri);
 
-        return axios.put(
-            url.href,
-            { values: [{ field_id, value: [file_id] }] },
-            {
-                headers: {
-                    "X-Auth-AccessKey": personal_access_key,
-                    "Content-type": "application/json",
-                },
-            }
-        );
+        return axios
+            .put(
+                url.href,
+                { values: [{ field_id, value: [file_id] }] },
+                {
+                    headers: {
+                        "X-Auth-AccessKey": personal_access_key,
+                        "Content-type": "application/json",
+                    },
+                }
+            )
+            .then(() => {
+                // Ignore the response
+            }, unwrapRestlerError);
     },
 });
